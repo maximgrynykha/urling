2284ad2caddf391ef1ee696daed39063280ca972
Maxim Grynykha <gr.gromax@gmail.com>
2021-03-19 01:45:23 +0200

    refactor(LogicVerifier.php): rename to Assertion.php

---
+++ src/Urling/Core/Utilities/Misc/Assertion.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Urling\Core\Utilities\Misc;
+
+/**
+ * @deprecated
+ */
+abstract class Assertion
+{
+    // ---------------------------------------------------------
+    // AND assertions
+    // ---------------------------------------------------------
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNotNullAndEmpty(&$context): bool
+    {
+        return !is_null($context) and empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNullAndEmpty(&$context): bool
+    {
+        return is_null($context) and empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNotNullAndNotEmpty(&$context): bool
+    {
+        return !is_null($context) and !empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNullAndNotEmpty(&$context): bool
+    {
+        return is_null($context) and !empty($context);
+    }
+
+    // ---------------------------------------------------------
+    // OR assertions
+    // ---------------------------------------------------------
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNotNullOrEmpty(&$context): bool
+    {
+        return !is_null($context) or empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNullOrEmpty(&$context): bool
+    {
+        return is_null($context) or empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNotNullOrOrEmpty(&$context): bool
+    {
+        return !is_null($context) or !empty($context);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param mixed $context
+     *
+     * @return bool
+     */
+    public static function isNullOrNotEmpty(&$context): bool
+    {
+        return is_null($context) or !empty($context);
+    }
+}
