e30366049164e6740a65ee64270ab622f1f10909
Maxim Grynykha <gr.gromax@gmail.com>
2021-03-27 06:43:33 +0200

    feat: add module - tools

---
+++ src/Urling/Core/Misc/Tools/Tools.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Urling\Core\Misc\Tools;
+
+use Urling\Core\Misc\Tools\Misc\NetWorker;
+use Urling\Core\Misc\Tools\Misc\SwissKnife;
+
+final class Tools
+{
+    use NetWorker;
+    use SwissKnife;
+}
--- src/Urling/Urling.php
+++ src/Urling/Urling.php
@@ -5,14 +5,17 @@
 namespace Urling;
 
 use Urling\Core\Url;
+use Urling\Core\Misc\Tools\Tools;
 
 # Entry point
 final class Urling
 {
     public Url $url;
+    public Tools $tools;
 
     public function __construct(string $url = null)
     {
         $this->url = new Url($url);
+        $this->tools = new Tools();
     }
 }
