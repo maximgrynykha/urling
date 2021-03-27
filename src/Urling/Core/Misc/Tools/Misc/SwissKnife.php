0195f185ccb96eda9af3284878c008a72e521a28
Maxim Grynykha <gr.gromax@gmail.com>
2021-03-27 08:34:26 +0200

    fix: multi-languaging & make static method createSlug()

--- src/Urling/Core/Misc/Tools/Misc/SwissKnife.php
+++ src/Urling/Core/Misc/Tools/Misc/SwissKnife.php
@@ -52,10 +52,10 @@ trait SwissKnife
      *
      * @return string
      */
-    public function createSlug(string $context, string $separator = " ", string $connector = "-"): string
+    public static function createSlug(string $context, string $separator = " ", string $connector = "-"): string
     {
         // Contains only letters in unicode and "$connector"
-        $prepared_context = preg_replace("/\PL{$connector}+|[^a-zA-Z]/u", $separator, $context);
+        $prepared_context = preg_replace("/\PL{$connector}+/iu", $separator, $context);
 
         // Replace all whitespaces to single whitespace
         $prepared_context = preg_replace("/\s+/", $separator, mb_strtolower(trim((string) $prepared_context)));
